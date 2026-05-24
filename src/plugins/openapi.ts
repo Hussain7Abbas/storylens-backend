d1e8ec1aa68df82b4b6ef18a71f7357cc527aaae
Hussain Abbas <hussain.jazing.21@gmail.com>
2026-05-24 10:55:40 +0300

    Refactor openapi plugin to enhance production handling and improve structure. Introduced onBeforeHandle method to return 404 status in production environment, while maintaining existing OpenAPI configuration.

--- src/plugins/openapi.ts
+++ src/plugins/openapi.ts
@@ -1,38 +1,45 @@
-import { openapi as elysiaOpenapi } from '@elysiajs/openapi';
-import { fromTypes } from '@elysiajs/openapi/gen';
-import { Elysia } from 'elysia';
-import { env } from '@/env';
+import { openapi as elysiaOpenapi } from "@elysiajs/openapi";
+import { fromTypes } from "@elysiajs/openapi/gen";
+import Elysia, { status } from "elysia";
+import { env } from "@/env";
 
-export const openapi =
-  env.NODE_ENV !== 'development'
-    ? new Elysia({})
-    : elysiaOpenapi({
-        references: fromTypes('src/server.ts'),
-        path: '/docs',
-        specPath: '/openapi.json',
-        documentation: {
-          tags: [],
-          servers: [
-            {
-              url: `{server}:${env.PORT}`,
-              description: 'Server URL',
-              variables: {
-                server: {
-                  default: 'http://localhost',
-                  description: 'The server URL',
-                  enum: ['http://localhost', 'https://storylens-api.iscoded.com'],
-                },
-              },
-            },
-          ],
-          components: {
-            securitySchemes: {
-              'Bearer Auth': {
-                type: 'http',
-                scheme: 'bearer',
-                bearerFormat: 'JWT',
-              },
-            },
-          },
-        },
-      });
+export const openapi = new Elysia({ name: "openapi" })
+	.onBeforeHandle(() => {
+		if (env.NODE_ENV === "production") {
+			return status(404);
+		}
+
+		return;
+	})
+	.use(
+		elysiaOpenapi({
+			references: fromTypes("src/server.ts"),
+			path: "/docs",
+			specPath: "/openapi.json",
+			documentation: {
+				tags: [],
+				servers: [
+					{
+						url: `{server}:${env.PORT}`,
+						description: "Server URL",
+						variables: {
+							server: {
+								default: "http://localhost",
+								description: "The server URL",
+								enum: ["http://localhost", "https://storylens-api.iscoded.com"],
+							},
+						},
+					},
+				],
+				components: {
+					securitySchemes: {
+						"Bearer Auth": {
+							type: "http",
+							scheme: "bearer",
+							bearerFormat: "JWT",
+						},
+					},
+				},
+			},
+		}),
+	);
